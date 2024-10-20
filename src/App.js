4bff65545c444860daba016b0a458aff3d28d098
hoangjam <j.hoag2080@gmail.com>
2024-10-20 10:48:08 -0400

    initial commit, some JS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import VideoPlayer from './pages/VideoPlayer';
+import Header from './components/Header';
 
 function App() {
   return (
